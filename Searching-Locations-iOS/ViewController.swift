5369be9c9f7ade1103c1b73f40d4764f03e23658
Ulul Ilmi <uilmi@na.edu>
2022-10-05 13:45:50 +0700

    first

---
+++ Searching-Locations-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Searching-Locations-iOS
+//
+//  Created by Ulul I. on 05/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
