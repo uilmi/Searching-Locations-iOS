bc332fa8d32c5b3e469993ede595a8ff6b95c858
Ulul Ilmi <uilmi@na.edu>
2022-10-13 15:05:36 +0700

    "Added CoreLocation"

--- Searching-Locations-iOS/ViewController.swift
+++ Searching-Locations-iOS/ViewController.swift
@@ -6,14 +6,53 @@
 //
 
 import UIKit
+import MapKit
+import FloatingPanel
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, SearchViewControllerDelegate {
 
+    let mapView = MKMapView()
+    let panel = FloatingPanelController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(mapView)
+        title = "TUJU"
+        
+        let searchVC = Searching_Locations_iOS.SearchViewController()
+        searchVC.delegate = self
+        
+        panel.set(contentViewController: searchVC)
+        panel.addPanel(toParent: self)
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        mapView.frame = view.bounds
+    }
+    
+    
+    func SearchViewController(_ vc: SearchViewController, didSelectLocationWith coordinates: CLLocationCoordinate2D?) {
+        
+        guard let coordinates = coordinates else {
+            return
+        }
 
+        panel.move(to: .tip, animated: true)
+        
+        mapView.removeAnnotations(mapView.annotations)
+        
+        let pin = MKPointAnnotation()
+        pin.coordinate = coordinates
+        mapView.addAnnotation(pin)
+        
+        mapView.setRegion(MKCoordinateRegion(center: coordinates, span: MKCoordinateSpan(
+            latitudeDelta: 0.7,
+            longitudeDelta: 0.7
+            )
+        ), animated: true)
+    }
 
 }
 
